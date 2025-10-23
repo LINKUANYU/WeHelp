3f723b3c08e51e18f5c5927de826825f388c382a
林冠宇 <linkuanyu@linguanyudeMacBook-Air.local>
2025-10-23 09:53:28 +0800

    10/23 10:00

--- week2/request1.js
+++ week2/request1.js
@@ -1,4 +1,9 @@
 function function1(name){
+    // find out pos of the name
+    // calculate the distance
+    // check section and decide if it needs extra point
+    // find the closest and farest
+    
     people = [
         {"name": "悟空", "pos": [0, 0]},
         {"name": "辛巴", "pos": [-3, 3]},
@@ -10,7 +15,8 @@ function function1(name){
     
     let section1 = ["悟空", "辛巴", "貝吉塔", "特南克斯"];
     let section2 = ["佛利沙", "丁滿"];
-    // calculate the differences of input name and each name
+    // find out pos of the name
+    
     for (let i = 0; i < people.length; i++){
         if (people[i].name == name){
             POS = people[i].pos;
@@ -18,62 +24,69 @@ function function1(name){
         }
     }
     // check section
-    function section(name){
+    function check_section(name){
         for (let i = 0; i < section1.length; i++){
             if (section1[i] === name){
                 return 1;
             }
         }
         for (let i = 0; i < section2.length; i++){
-            return 2;
+            if (section2[i] === name){
+                return 2;
+            }
         }
         return console.log("Not Found");
     }
 
-    let target_section = section(name);
+    let target_section = check_section(name);
+
+    // calculate the distance
+    let extrapoint = 2;
     let new_list = [];
     for (let i = 0; i < people.length; i++){
+        // skip itself
         if (people[i].name == name){
             continue;
         }
-        else if (section(people[i].name) === target_section){
+        // same section
+        else if (check_section(people[i].name) === target_section){
             distance_raw = Math.abs(people[i].pos[0] - POS[0]) + Math.abs(people[i].pos[1] - POS[1]);
         }
-        else if (section(people[i].name) != target_section){
-            distance_raw = Math.abs(people[i].pos[0] - POS[0]) + Math.abs(people[i].pos[1] - POS[1]) + 2;
+        // different section
+        else if (check_section(people[i].name) != target_section){
+            distance_raw = Math.abs(people[i].pos[0] - POS[0]) + Math.abs(people[i].pos[1] - POS[1]) + extrapoint;
         }
-        new_list.push({[people[i].name] : distance_raw})
+        // make new list to put result
+        new_list.push({[people[i].name] : distance_raw});
     }
 
+    // find the closest
     let min = Infinity;
     let min_list = [];
     for (o of new_list){
         [name, value] = Object.entries(o)[0]
         if (value < min){
             min = value;
-            min_list = [name];
+            min_list = [name]; // don't use push. new value should overwrite everything.
         }
         else if (value === min){
             min_list.push(name);
         }
     }
+    //find the farest
     let max = -Infinity;
     let max_list = [];
     for (o of new_list){
         [name, value] = Object.entries(o)[0];
         if (value > max){
             max = value;
-            max_list = [name];
+            max_list = [name];  // don't use push. new value should overwrite everything.
         }
         else if(value === max){
             max_list.push(name);
         }
     }
     console.log("最遠", max_list.join("、"), "; 最近", min_list.join("、"));
-
-    // check every name if they are  in the same section with input name
-
-    // find the farest and closest one and print
     
 }
 
