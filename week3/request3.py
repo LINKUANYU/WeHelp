a2599c112370a2b21a7dc85501fd723304b3081b
林冠宇 <linkuanyu@linguanyudeMacBook-Air.local>
2025-10-29 14:05:29 +0800

    10/29 1400

--- week3/request3.py
+++
@@ -1,8 +0,0 @@
-import urllib.request as req
-import json
-url1 = "https://cwpeng.github.io/test/assignment-3-1"
-url2 = "https://cwpeng.github.io/test/assignment-3-2"
-with req.urlopen(url1) as response:
-    data1_raw = response.read().decode("utf-8")
-data = json.loads(data1_raw)
-print(len(data["rows"]))
