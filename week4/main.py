4d92a2dbf529c3456efd8277ab1189b5753bacfd
林冠宇 <linkuanyu@linguanyudeMacBook-Air.local>
2025-11-03 18:01:17 +0800

    11/3 1800

--- week4/main.py
+++ week4/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from typing import Annotated
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -10,10 +12,32 @@ app.mount("/static", StaticFiles(directory="static"), name = "static")
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+@app.post("/login")
+def login(
+    request: Request,
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()]
+    ):
+    if not email or not password:
+        return RedirectResponse(
+            url="/ohoh?msg=請輸入信箱和密碼",
+            status_code=303
+        )
+        
+    elif email != "abc@abc.com" or password != "abc":
+        return RedirectResponse(
+            url="/ohoh?msg=信箱或密碼輸入錯誤",
+            status_code=303
+        )
+    
+    return RedirectResponse(url="/member", status_code=303)
 @app.get("/member")
 def member(request: Request):
     return templates.TemplateResponse("member.html", {"request": request})
 
 @app.get("/ohoh")
-def member(request: Request):
-    return templates.TemplateResponse("ohoh.html", {"request": request})
+def ohoh(
+     request: Request,
+     msg: str = None
+     ):
+    return templates.TemplateResponse("ohoh.html", {"request": request, "msg":msg})
