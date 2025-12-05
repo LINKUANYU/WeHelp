66c9b8332535178db899d3901493a46bf00cbed9
kaun <kuan@gmail.com>
2025-12-05 11:59:20 +0800

    task5 XSS prevention done

--- week8/task5/reflected_xss.py
+++ week8/task5/reflected_xss.py
@@ -1,16 +1,55 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-templates = Jinja2Templates(directory=".")
 
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("reflected_xss.html", {"request": request})
-
+# Reflected_XSS 概念：正常的網站搜尋頁因設計漏洞，被攻擊者利用
 @app.get("/search")
-def search(request: Request, q: str = ""):
-    return templates.TemplateResponse("reflected_xss.html", {"request": request, "q": q})
+def search(q: str = ""):
+    html = f"""
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="/base.css">
+            <title>Reflected XSS Demo</title>
+        </head>
+        <body>
+            <header class="l-header flex">week8任務：Reflected XSS Demo</header>
+            <div class="card">
+                <h3 class="text-center">正常的網站搜尋頁(網站設計有漏洞)</h3>
+                <form class="flex" action="/search">
+                    <input name="q" autocomplete="off">
+                    <button type="submit">搜尋</button>
+                </form>
+                <div class="text-center">你搜尋了：{q}</div>
+            </div>
+        </body>
+    </html>
+    """
+    return HTMLResponse(html)
+
+# 最後將有問題的網址散播在任何地方給受害者點
+# http://127.0.0.1:8000/search?q=%3Cscript%3Ealert(%27XSS%20ATTACK%27)%3C/script%3E
+
+
+
+# # 防禦概念：不要自己用字串拼 HTML，改用模板引擎有預設 HTML escape 功能
+# # jinja engine
+
+# templates = Jinja2Templates(directory=".")
+
+# @app.get("/search")
+# def search(request: Request, q: str = ""):
+#     return templates.TemplateResponse("reflected_xss.html", {"request": request, "q": q})
 
 app.mount("/", StaticFiles(directory="."))
+
+# HTML escape：在 HTML 裡，把這些符號有「語法上的意義」變成「安全的文字」來顯示
+# 把 < 變成 &lt;          <：開始一個標籤
+# 把 > 變成 &gt;          >：結束一個標籤
+# 把 & 變成 &amp;         &：開始一個實體（例如 &nbsp;）
+# 把 " 變成 &quot;        " / '：用在屬性的引號裡
+# 把 ' 變成 &#x27;（或 &#39;）
--- week8/task5/stored_xss.py
+++ week8/task5/stored_xss.py
@@ -1,19 +1,61 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 
-app = FastAPI()
-templates = Jinja2Templates(directory=".")
 # 把留言暫時存進記憶體，server關掉後消失
 comment = []
+
+app = FastAPI()
+
+# # Stored_XSS 概念：資料庫設計漏洞，攻擊者存入惡意腳本
+# @app.get("/")
+# def home():
+#     comment_html = comment_html = "<br>".join(comment) or "目前沒有留言"
+
+#     html = f"""
+#     <html lang="en">
+#         <head>
+#             <meta charset="UTF-8">
+#             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#             <link rel="stylesheet" href="/base.css">
+#             <title>Storted XSS Demo</title>
+#         </head>
+#         <body>
+#             <header class="l-header flex">week8任務：Stored XSS Demo</header>
+#             <!-- Demo -->
+#             <div class="card">
+#                 <p class="text-center">留言板(設計有漏洞)</p>
+#                 <form class="flex" action="/comment" method="post">
+#                     <input name="content" autocomplete="off">
+#                     <button type="submit">留言</button>
+#                 </form>
+#                 <div class="text-center">留言列表：</div>
+#                 <div class="flex">
+#                     {comment_html}
+#                 </div>
+#             </div>
+#         </body>
+#     </html>
+#     """
+#     return HTMLResponse(html)
+# # 攻擊者直接輸入惡意程式存進資料庫，每當有人查看資料就會跑出來
+# # <script>alert("XSS ATTACK")</script> 
+
+# 防禦概念：
+
+# 資料庫裡的東西一律當「不可信」 就算是很久以前存的、自己系統產生的，也可能被利用。
+# 輸出到 HTML 時，一樣要用模板引擎 + auto escape。
+# jinja engine
+templates = Jinja2Templates(directory=".")
 
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("stored_xss.html", {"request": request, "comment": comment})
 
+# add comment
 @app.post("/comment")
-def search(content: str = Form(...)):
+def add_comment(content: str = Form(...)):
     comment.append(content)
     return RedirectResponse("/", status_code=303)
 
